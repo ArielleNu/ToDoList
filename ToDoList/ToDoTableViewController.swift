0cecda22ae458ed3006cb295b92e65991a91ef87
Arielle Nudelman <ariellenudelman2@gmail.com>
2022-07-18 20:26:14 -0400

    Complete Iteration 1

--- ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoTableViewController.swift
@@ -50,12 +50,26 @@ class ToDoTableViewController: UITableViewController {
       return cell
     }
 
-    /*
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+      if let addVC = segue.destination as? AddToDoViewController {
+        addVC.previousVC = self
+      }
+
+      if let completeVC = segue.destination as? CompleteToDoViewController {
+        if let toDo = sender as? ToDo {
+          completeVC.selectedToDo = toDo
+          completeVC.previousVC = self
+        }
+      }
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+      // this gives us a single ToDo
+      let toDo = toDos[indexPath.row]
+
+      performSegue(withIdentifier: "moveToComplete", sender: toDo)
+    }
 
 }
 
